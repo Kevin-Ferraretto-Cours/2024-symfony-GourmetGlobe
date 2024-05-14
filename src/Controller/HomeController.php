efad390b27c408e454eae2deac1e2666d7c29fc6
MrSuricate <kevindu032270@gmail.com>
2024-05-14 10:57:17 +0200

    Ajout de la possibilité de commenter et l'affichage des commentaire

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Recipe;
+use App\Form\CommentType;
 use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,11 +22,29 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/recipe/{id}', name: 'app_home_recipe')]
-    public function recipe(Recipe $recipe): Response
+    #[Route('/recipe/{id}', name: 'app_home_recipe', methods: ['GET', 'POST'])]
+    public function recipe(Recipe $recipe, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setUser($this->getUser());
+            $comment->setDate(new \DateTime());
+            $recipe->addComment($comment);
+            $entityManager->persist($comment);
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+
+            return $this->redirectToRoute('app_home_recipe', ['id' => $recipe->getId()], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('home/show.html.twig', [
             'recipe' => $recipe,
+            'comments' => $recipe->getComment(),
+            'form' => $form,
         ]);
     }
 }
