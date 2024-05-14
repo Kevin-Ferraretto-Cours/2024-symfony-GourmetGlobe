62ed9a5b3c319a809a5b36cab4a77fc0ad75fef9
MrSuricate <kevindu032270@gmail.com>
2024-05-14 11:04:10 +0200

    Ajout de la sécurité des roles

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -11,9 +11,11 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 
 #[Route('profile/recipe')]
+#[IsGranted('ROLE_USER')]
 class RecipeController extends AbstractController
 {
     #[Route('/', name: 'app_recipe_index', methods: ['GET'])]
