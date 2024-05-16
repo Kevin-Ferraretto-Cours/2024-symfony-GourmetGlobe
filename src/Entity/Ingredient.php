a49c64f4fb0019edb6e2062a7ed86ae8e86ce5ef
MrSuricate <kevindu032270@gmail.com>
2024-05-16 08:47:07 +0200

    Ajout de nom unique pour les ingredient

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -6,8 +6,11 @@ use App\Repository\IngredientRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_NAME', fields: ['name'])]
+#[UniqueEntity(fields: ['name'], message: 'Il existe déjà un ingrédient avec ce nom')]
 class Ingredient
 {
     #[ORM\Id]
